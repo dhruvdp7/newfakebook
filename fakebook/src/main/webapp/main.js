13eb395d16780bd2f5aa6c1ac3e8a335f81ccacc
dhruvdp7 <46739476+dhruvdp7@users.noreply.github.com>
2019-02-15 16:08:10 +0530

    Add files via upload

--- fakebook/src/main/webapp/main.js
+++ fakebook/src/main/webapp/main.js
@@ -9,6 +9,7 @@ function init() {
 	btn3.addEventListener('click', addBtn3Click);
 	// main =document.querySelector('#main');
 }
+
 function addBtn1Click() {
 
 	$(".comment").remove();
@@ -22,7 +23,7 @@ function addBtn1Click() {
 	var submit_container = document.createElement('div');
 	var submit = document.createElement('input');
 
-	form.setAttribute('action', 'webapi/commentThis page is displayed in an iframe/addcomment');
+	form.setAttribute('action', 'webapi/comment/addcomment');
 	form.setAttribute('method', 'post');
 	comment_area.setAttribute('rows', '4');
 	comment_area.setAttribute('cols', '50');
@@ -46,63 +47,67 @@ function addBtn1Click() {
 
 
 
-function getComment(){
-	 return $.ajax({
-	        url: 'http://localhost:8080/fakebook/webapi/comment/getcomment',
-	        dataType: 'json',
-	        success: function (data) {
-	            return data;
-	        }
-	    });
-}
-
 function addBtn2Click() {
 	$(".comment").remove();
 	$("#main").append(" <div class='formcontainer'>" +
-			"<form action='http://localhost:8080/fakebook/webapi/comment/getcomment'" +
-			"method='get' id='form1' target='_blank'></form></div>");
+			"</div>");
 	// $("#main").append("<iframe src='index.html' name='myframe' height='200px'
 	// width='200px'>");
 	
-	$(".formcontainer").append(" <div class='emailcontainer'><input type='email' name='email' " +
-			"placeholder='Enter Email..' form='form1' required></input></div>");
+	$(".formcontainer").append(" <div class='emailcontainer'><input type='email' name='email' id='user-email' " +
+			"placeholder='Enter Email..' required></div>");
 	
 	$(".formcontainer").append(" <div class='buttoncontainer'><input type='submit' id='btnsubmit' " +
-			"value='Get Comments' form='form1' ></input></div>");
+			"value='Get Comments'></div>");
 
-	
-	$("#btnsubmit").click(function(event) {
-		
-		
-		event.preventDefault();
+			$("#btnsubmit").click(()=>{
+				let email = $("#user-email")[0].value
+				appendComments(email);
+			})
+}
+
+
+function appendComments(email){
+
+		// $('#form1').submit();
 		   $('.buttoncontainer').hide();
 		  $('.emailcontainer').hide();
 		$(".heading1").remove();
 		$(".heading2").remove();
-		 $("#main").append(" <div class='heading2'><h1>User Comments</h1></div>");
+		 $("#main").append(" <div class='heading2'><h2>Comments</h2></div>");
+			$('#main').append("<div class='emailname'><h3>By</h3>"+"<h3>"+email+"</h3></div>");	
 		$("#main").append(" <div class='commentcontainer'></div>");
-			
-		getComment().then(data => {
+	
+		getComment(email).then(data => {
 			console.log(data);
-			$('.commentcontainer').append("<div class='all-email'><h3></h3></div>");
-			//console.log(data.comment[0].email);
-			console.log("before loop");
+	
+			
+		
 			for (let i=0;Object.keys(data).length;i++){
 				
 				$('.commentcontainer').append("<div class='row'></div>");
 				 $('.row:eq('+i+')').append("<div class='all-comment'></div>");
 				 
 				 
-				 		// "<div class='all-email'></div></div>");
-				 $('.all-comment:eq('+i+')').append(data.comment[i].comment);
-				 //$('.all-email:eq('+i+')').append("<h5>Email:</h5>"+data.comments[i].email);
+				 		
+				 $('.all-comment:eq('+i+')').append(data.comments[i].comment);
+				 // $('.all-email:eq('+i+')').append("<h5>Email:</h5>"+data.comments[i].email);
 				 $('.commentcontainer').append("<hr>");
 			}                                                                                                                                                                                                                                                
 		}).catch(err => console.log('error occured'));	
 		
-			
-		  
-});
+}
+
+
+
+function getComment(email){
+	console.log(email)
+	return $.ajax({
+		   url: 'http://localhost:8080/fakebook/webapi/comment/getcomment?email='+email,
+		   success: function (data) {
+			   return data;
+		   }
+	   });
 }
 
 
@@ -118,6 +123,8 @@ function getComments(){
 	        }
 	    });
 }
+
+
 function addBtn3Click() {
 	$(".comment").remove();
 	$(".heading1").remove();
@@ -130,15 +137,15 @@ getComments().then(data => {
 	
 		$('.commentcontainer').append("<div class='row'></div>");
 		 $('.row:eq('+i+')').append("<div class='all-comment'></div> " +
-		 		"<div class='all-email'></div>");
-		 
-		 
-		 		// "<div class='all-email'></div></div>");
+		 		"<div class='all-email'></div>");	
 		 $('.all-comment:eq('+i+')').append("<h5>Comment:</h5>"+data.comments[i].comment);
 		 $('.all-email:eq('+i+')').append("<h5>Email:</h5>"+data.comments[i].email);
 		 $('.commentcontainer').append("<hr>");
 	}                                                                                                                                                                                                                                                
 }).catch(err => console.log('error occured'));
 }
+
+
+
 
 
